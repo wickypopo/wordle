e237c547e65480365fef0a4afa6521c4f922d77f
wickypopo <felixm@meise.at>
2026-05-06 20:53:33 +0200

    tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { style } from "motion/react-client";
 import Confetti from "react-confetti-boom";
 
 function App() {
-  console.log("render");
   //
   // Tastatur && Spielfeld Data
 
@@ -150,12 +149,18 @@ function App() {
 
   const keyboard = keyboardrows.map((row) => {
     return (
-      <div className="key-row" key={nanoid()}>
+      <div
+        className="flex flex-row gap-2 justify-center w-[95vw]"
+        key={nanoid()}
+      >
         {row.map((key) => {
           if (key.key === "backspace") {
             return (
               <button
-                className={key.key}
+                className={
+                  key.key +
+                  " flex justify-center items-center text-white sm:w-20 h-14 bg-zinc-500 rounded-md px-2"
+                }
                 onClick={() => handleKeyboardDelete(lastKey)}
                 key={nanoid()}
               >
@@ -165,17 +170,23 @@ function App() {
           } else if (key.key === "enter") {
             return (
               <button
-                className={key.key}
+                className={
+                  key.key +
+                  " text-white sm:w-20 h-14 bg-zinc-500 rounded-md font-bold text-xs sm:text-[16px] px-2"
+                }
                 onClick={() => handleKeyboardSubmit(key.key)}
                 key={nanoid()}
               >
-                {key.key.toLocaleUpperCase()}
+                {key.key}
               </button>
             );
           } else {
             return (
               <button
-                className={key.status}
+                className={
+                  key.status +
+                  " text-white w-full sm:w-14 h-14 bg-zinc-500 rounded-md font-bold"
+                }
                 onClick={() => handleKeyboard(key.key)}
                 key={nanoid()}
               >
@@ -189,9 +200,16 @@ function App() {
   });
 
   const playingBoard = rows.map((row) => (
-    <div className="key-row" key={nanoid()}>
+    <div className="cell-row" key={nanoid()}>
       {row.map((cell) => (
-        <div className={cell.status + " cell"} key={cell.id} id={cell.id}>
+        <div
+          className={
+            cell.status +
+            " flex justify-center items-center text-white text-xl font-bold w-14 h-14 border-2 border-zinc-800 rounded-md"
+          }
+          key={cell.id}
+          id={cell.id}
+        >
           {cell.letter}
         </div>
       ))}
@@ -375,20 +393,33 @@ function App() {
   }, []);
 
   return (
-    <main>
+    <main className="bg-zinc-950">
       <Toaster position="top-center" reverseOrder={false} />
       {/* ================================= */}
-      {win || loss ? null : <h1>Wordle</h1>}
+      {win || loss ? null : (
+        <h1 className="text-zinc-50 text-4xl text-700">Wordle</h1>
+      )}
 
       {win ? <Confetti /> : null}
-      {win ? <h1>You Won!</h1> : null}
+      {win ? (
+        <h1 className="text-zinc-50 text-4xl text-700">You Won!</h1>
+      ) : null}
       {loss ? <Confetti /> : null}
-      {loss ? <h1>You Lost, the right word was {randomWord}</h1> : null}
+      {loss ? (
+        <h1 className="text-zinc-50 text-4xl text-700">
+          You Lost, the right word was {randomWord}
+        </h1>
+      ) : null}
       {/* ================================= */}
-      <div className="keyboard">{playingBoard}</div>
-      <div className="keyboard">{keyboard}</div>
+      <div className="cells">{playingBoard}</div>
+      <div className="flex flex-col gap-2">{keyboard}</div>
       {win || loss ? (
-        <button onClick={startNewGame}>Start new game</button>
+        <button
+          className="bg-violet-600 text-white rounded-md font-bold p-4"
+          onClick={startNewGame}
+        >
+          Start new game
+        </button>
       ) : null}
       {/* 
       <button
@@ -398,7 +429,7 @@ function App() {
       >
         Aktualisieren
       </button>
-      <p>{randomWord ? randomWord : "Loading..."}</p>
+      <p className="text-white">{randomWord ? randomWord : "Loading..."}</p>
       */}
     </main>
   );
